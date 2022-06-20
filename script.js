720af2c432ff9a5fa117859c8fd73be68d8fe6b2
Alex <alexkravchenko27@gmail.com>
2022-06-20 12:43:22 +0300

    dasdas

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-let partOneFirstChoise = document.querySelector('.part_1 .one');
-let partOneSecondChoise = document.querySelector('.part_1 .two');
-let partOneLastChoise = document.querySelector('.part_1 .three');
+const textNodes = [
+  {
+    id: 1,
+    text: 'firstText',
+    option: [
+      {
+        textBtn: 'choise one',
+        nextText: 2,
+      },
+      {
+        textBtn: 'choise two',
+        nextText: 2,
+      }
+    ]
+  },
+  {
+    id: 2,
+    text: 'secondText',
+    option: [
+      {
+        textBtn: 'choise two',
+        nextText: 3,
+      }
+    ]
+  }
+];
 
-let partTwoFirstChoise = document.querySelector('.part_2 .one');
-let partTwoSecondChoise = document.querySelector('.part_2 .two');
-let partTwoLastChoise = document.querySelector('.part_2 .three');
+let textElement = document.querySelector('.text');
+let btns = document.querySelector('.buttons');
 
-let partOne = document.querySelector('.part_1');
-let partTwo = document.querySelector('.part_2');
-let partTwoOne = document.querySelector('.part_2-1');
-let partTwoTwo = document.querySelector('.part_2-2');
-let partTwoThree = document.querySelector('.part_2-3');
-let partThree = document.querySelector('.part_3');
+function showText(text) {
+  for (let i = 0; i < textNodes.length; i++) {
+    console.log(textNodes[i]);
+    for (let k = 0; k < textNodes[i].option.length; k++) {
+      console.log(textNodes[i].option)
+    }
+  }
 
-partOneFirstChoise.addEventListener('click', () => {
-  alert('you die');
-  partOne.classList.add('disable');
-});
+};
 
-partOneSecondChoise.addEventListener('click', () => {
-  partOne.classList.add('disable');
-  partTwo.classList.remove('disable');
-});
-
-partOneLastChoise.addEventListener('click', () => {
-  partOne.classList.add('disable');
-  partThree.classList.remove('disable');
-});
-
-partTwoFirstChoise.addEventListener('click', () => {
-  partTwo.classList.add('disable');
-  partTwoOne.classList.remove('disable');
-});
-
-partTwoSecondChoise.addEventListener('click', () => {
-  partTwo.classList.add('disable');
-  partTwoTwo.classList.remove('disable');
-})
-
-partTwoLastChoise.addEventListener('click', () => {
-  partTwo.classList.add('disable');
-  partTwoThree.classList.remove('disable');
-})
+showText(textNodes);
