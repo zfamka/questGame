f2fedd262c7c9ba57b5ffefe8e2bc52f2b8674b8
Alex <alexkravchenko27@gmail.com>
2022-07-16 13:09:09 +0300

    fix menu sound

--- js/start.js
+++ js/start.js
@@ -7,12 +7,22 @@ const startGameAudio = document.querySelector('.start__game-audio');
 const btn = document.querySelectorAll('.btn');
 
 startGames.addEventListener('click', () => {
+  const mainMenuOst = document.querySelector('.main__menu-ost');
+  let count = 1;
+  let count2 = 1;
   secondLogo.style.opacity = '0';
   firstLogo.style.opacity = '1';
   firstLogo.style.display = 'block';
   startGames.style.animation = 'fonstSize 2.5s 1';
   startGames.style.transition = '1.5s'
   startGames.style.opacity = '1';
+
+  setInterval(() => {
+    count = count - 0.1;
+    mainMenuOst.volume = count;
+    startGameAudio.volume = count2;
+  }, 380);
+
   startGameAudio.play();
 
   btn.forEach(elem => {
@@ -20,10 +30,9 @@ startGames.addEventListener('click', () => {
   })
 
   setTimeout(() => {
-    body.classList.add('hideElem')
+    body.classList.add('hideElem');
     setTimeout(() => {
       window.location.href = 'gamePages/game.html';
-
     }, 2500);
 
   }, 1500);
