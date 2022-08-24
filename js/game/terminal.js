cd54c3a9b9436c7828f3431867a24769a5d508dc
Alex <alexkravchenko27@gmail.com>
2022-08-24 15:52:12 +0300

    add terminal

---
+++ js/game/terminal.js
@@ -0,0 +1,43 @@
+const terminal = ['Планета Чіпуюхі.', 'Десь у галактиці Телепісі', 'Час та пора року невідома.', 'Пошкоджено генератор основного блоку через зіткнення з астероїдом.', 'Спостерігається стрімке зниження кисню у повітрі та перебої із світлом', 'Температура - 22oС', 'Кисень - 97 %', 'Освітленність - 60 %'];
+
+
+
+const startTerminal = document.querySelector('.preHistory__startTerminal');
+const terminalText = document.querySelector('.preHistory__terminal');
+
+startTerminal.addEventListener('click', () => {
+  startTerminal.style.display = 'none';
+  terminalText.style.display = 'block';
+  function typeText() {
+    let line = 0;
+    let count = 0;
+    let out = '';
+    let terminalWindow = document.querySelector('.preHistory__terminal');
+
+    function typeLine() {
+      let interval = setTimeout(function () {
+        out += terminal[line][count];
+        terminalWindow.innerHTML = out;
+        count++;
+        if (count >= terminal[line].length) {
+          count = 0;
+          line++;
+          if (line == terminal.length) {
+            clearTimeout(interval);
+            return true
+          }
+        }
+        typeLine();
+      }, 200);
+    }
+    typeLine();
+  }
+
+  typeText();
+})
+
+
+
+
+
+
