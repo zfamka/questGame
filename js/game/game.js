82f64fff755e81fee94bec4a2d99d0e43e840ecc
Alex <alexkravchenko27@gmail.com>
2022-09-06 19:53:51 +0300

    blur fix

--- js/game/game.js
+++ js/game/game.js
@@ -43,8 +43,10 @@ function showTextNode(textNodeIndex) {
   //----------------- Elea photo ID 4
   if (textNode.id === 4) {
     eleaPhoto.style.display = 'block';
+    optionButtonsElement.style.opacity = '0';
     setTimeout(() => {
       eleaPhoto.style.opacity = '0';
+      optionButtonsElement.style.opacity = '1';
     }, 5000);
     setTimeout(() => {
       eleaPhoto.style.display = 'none';
