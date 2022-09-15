e4c0e3945afbfcffe86189d55aa51d5cefbd4a58
Alex <alexkravchenko27@gmail.com>
2022-09-15 17:21:53 +0300

    invetory window

--- js/game/inventory.js
+++ js/game/inventory.js
@@ -1,8 +1,23 @@
 const itemsBracer = document.querySelector('.itemsBracer');
+const inventoryMenu = document.querySelector('.inventory__menu');
 
+let inventoryMenuToogle = false;
 itemsBracer.addEventListener('click', () => {
+
+  if (inventoryMenuToogle === false) {
+    inventoryMenu.style.left = '0px';
+    inventoryMenuToogle = true;
+  } else {
+    inventoryMenu.style.left = '-300px';
+    inventoryMenuToogle = false;
+  }
+
+
   itemsBracer.style.transform = 'scale(1)';
   setTimeout(() => {
     itemsBracer.style.transform = 'scale(1.1)';
-  }, 150);
+  }, 100);
 });
+
+
+
