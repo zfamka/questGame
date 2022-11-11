256b58dd2c7a298fc516ab52bc390e48d552c323
Alex <alexkravchenko27@gmail.com>
2022-11-11 21:11:23 +0200

    add time for eventActive func

--- js/game/eventAnimation.js
+++ js/game/eventAnimation.js
@@ -4,8 +4,15 @@ function eventAnimation(textNode, animationTime, blockAnimation, imgAnimation, s
     splashFire.style.animation = `${animationTime} linear ${blockAnimation}`;
     document.querySelector('.activeEvent__img').src = `${srcImage}`
     document.querySelector('.activeEvent__img').style.animation = `${animationTime} linear ${imgAnimation}`;
+
+    let stringToNumber = animationTime.split('');
+    stringToNumber.pop();
+    stringToNumber.pop();
+    animationTime = stringToNumber.toString();
+    animationTime = animationTime.replace(/,/g, '');
+
     setTimeout(() => {
       splashFire.style.display = 'none';
-    }, 400);
+    }, animationTime);
   }
 }
