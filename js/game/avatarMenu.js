05ae468f0007f2823dc1db2538fae4021a6c764b
Alex <alexkravchenko27@gmail.com>
2022-11-23 13:08:36 +0200

    some fix

--- js/game/avatarMenu.js
+++ js/game/avatarMenu.js
@@ -1,31 +1,31 @@
-document.querySelector('.characterInfo__temp').addEventListener('mousedown', () => {
+document.querySelector('.characterInfo__temp').addEventListener('mouseover', () => {
   const newDiv = document.createElement("div");
   newDiv.classList.add('statHint');
   newDiv.innerHTML = 'some hint for players';
   document.querySelector('.characterInfo__temp').appendChild(newDiv)
-  document.querySelector('.characterInfo__temp').addEventListener('mouseup', () => {
+  document.querySelector('.characterInfo__temp').addEventListener('mouseout', () => {
     newDiv.remove();
   })
 })
 
-document.querySelector('.characterInfo__wet').addEventListener('mousedown', () => {
+document.querySelector('.characterInfo__wet').addEventListener('mouseover', () => {
   const newDiv = document.createElement("div");
   newDiv.classList.add('statHint');
   newDiv.innerHTML = 'some hint for players';
   newDiv.style.right = '80px';
   document.querySelector('.characterInfo__temp').appendChild(newDiv)
-  document.querySelector('.characterInfo__wet').addEventListener('mouseup', () => {
+  document.querySelector('.characterInfo__wet').addEventListener('mouseout', () => {
     newDiv.remove();
   })
 })
 
-document.querySelector('.characterInfo__stamina').addEventListener('mousedown', () => {
+document.querySelector('.characterInfo__stamina').addEventListener('mouseover', () => {
   const newDiv = document.createElement("div");
   newDiv.innerHTML = 'some hint for players';
   newDiv.classList.add('statHint');
   newDiv.style.right = '-15px';
   document.querySelector('.characterInfo__temp').appendChild(newDiv)
-  document.querySelector('.characterInfo__stamina').addEventListener('mouseup', () => {
+  document.querySelector('.characterInfo__stamina').addEventListener('mouseout', () => {
     newDiv.remove();
   })
 })
